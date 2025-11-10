398b870757cf54ae8e17361fbed45b72111c80e7
Prashant Shrestha <axiosprashant@gmail.com>
2025-11-10 16:49:56 +0545

    Remove basePath for root domain deployment

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
   images: {
     unoptimized: true,
     domains: ["cdn.sanity.io", "zomeru.com", "raw.githubusercontent.com"],
