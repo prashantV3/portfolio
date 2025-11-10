5a078c6b3b719494916119c37ef63f81e96c6e96
Prashant Shrestha <axiosprashant@gmail.com>
2025-11-10 16:42:49 +0545

    Add basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
   images: {
     unoptimized: true,
     domains: ["cdn.sanity.io", "zomeru.com", "raw.githubusercontent.com"],
